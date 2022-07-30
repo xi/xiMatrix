87e361a8923e8645db92b8a546d5fb4ad62c5ee4
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-07-30 09:24:40 +0200

    display requests

--- popup.js
+++ popup.js
@@ -1,5 +1,25 @@
 /* global browser */
 
+var table = document.querySelector('table');
+
 browser.runtime.sendMessage({type: 'get'}).then(requests => {
-    console.log(requests);
+    for (const hostname in requests) {
+        for (const type in requests[hostname]) {
+            const tr = document.createElement('tr');
+
+            let td = document.createElement('td');
+            td.textContent = hostname;
+            tr.append(td);
+
+            td = document.createElement('td');
+            td.textContent = type;
+            tr.append(td);
+
+            td = document.createElement('td');
+            td.textContent = requests[hostname][type];
+            tr.append(td);
+
+            table.append(tr);
+        }
+    }
 });
