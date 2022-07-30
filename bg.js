f78890927f580b9137d78313c2ff2e9edbbfbe92
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-07-30 09:13:09 +0200

    collect requests for each tab

--- bg.js
+++ bg.js
@@ -1,1 +1,41 @@
 /* global browser */
+
+var requests = {};
+
+var getHostname = function(url) {
+    var u = new URL(url);
+    return u.hostname;
+};
+
+var pushRequest = function(tabId, hostname, type) {
+    if (!requests[tabId]) {
+        requests[tabId] = {};
+    }
+    if (!requests[tabId][hostname]) {
+        requests[tabId][hostname] = {};
+    }
+    if (!requests[tabId][hostname][type]) {
+        requests[tabId][hostname][type] = 0;
+    }
+    requests[tabId][hostname][type] += 1;
+};
+
+var clearRequests = function(tabId) {
+    if (requests[tabId]) {
+        delete requests[tabId];
+    }
+};
+
+browser.tabs.onRemoved.addListener(clearRequests);
+browser.webNavigation.onBeforeNavigate.addListener(details => {
+    clearRequests(details.tabId);
+});
+
+browser.webRequest.onBeforeRequest.addListener(details => {
+    if (details.type === 'main_frame') {
+        return;
+    }
+
+    var hostname = getHostname(details.url);
+    pushRequest(details.tabId, hostname, details.type);
+}, {urls: ['<all_urls>']});
