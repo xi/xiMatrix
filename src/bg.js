900fced1b4d7bf350aedef3caaae94d08f54a774
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-10-15 13:43:37 +0200

    avoid state in background page

--- src/bg.js
+++ src/bg.js
@@ -1,38 +1,45 @@
 /* global browser shared */
 
-var rules = {};
-var requests = {};
-
 var getHostname = function(url) {
     var u = new URL(url);
     return u.hostname;
 };
 
-var setRule = function(context, hostname, type, rule) {
-    if (hostname === 'first-party') {
-        context = '*';
-    }
-    if (!rules[context]) {
-        rules[context] = {};
-    }
-    if (!rules[context][hostname]) {
-        rules[context][hostname] = {};
-    }
-    if (rule) {
-        rules[context][hostname][type] = rule;
-    } else {
-        delete rules[context][hostname][type];
-        if (Object.keys(rules[context][hostname]).length === 0) {
-            delete rules[context][hostname];
-        }
-        if (Object.keys(rules[context]).length === 0) {
-            delete rules[context];
-        }
-    }
-    browser.storage.local.set({'rules': rules});
+var getRules = function() {
+    return browser.storage.local.get('rules').then(data => data.rules || {});
 };
 
-var restrictRules = function(context) {
+var getRequests = function() {
+    return browser.storage.local.get('requests').then(data => data.requests || {});
+};
+
+var setRule = function(context, hostname, type, rule) {
+    return getRules().then(rules => {
+        if (hostname === 'first-party') {
+            context = '*';
+        }
+        if (!rules[context]) {
+            rules[context] = {};
+        }
+        if (!rules[context][hostname]) {
+            rules[context][hostname] = {};
+        }
+        if (rule) {
+            rules[context][hostname][type] = rule;
+        } else {
+            delete rules[context][hostname][type];
+            if (Object.keys(rules[context][hostname]).length === 0) {
+                delete rules[context][hostname];
+            }
+            if (Object.keys(rules[context]).length === 0) {
+                delete rules[context];
+            }
+        }
+        return browser.storage.local.set({'rules': rules});
+    });
+};
+
+var restrictRules = function(rules, context) {
     var restricted = {};
     restricted['*'] = rules['*'] || {};
     restricted[context] = rules[context] || {};
@@ -40,22 +47,28 @@ var restrictRules = function(context) {
 };
 
 var pushRequest = function(tabId, hostname, type) {
-    if (!requests[tabId]) {
-        requests[tabId] = {};
-    }
-    if (!requests[tabId][hostname]) {
-        requests[tabId][hostname] = {};
-    }
-    if (!requests[tabId][hostname][type]) {
-        requests[tabId][hostname][type] = 0;
-    }
-    requests[tabId][hostname][type] += 1;
+    return getRequests().then(requests => {
+        if (!requests[tabId]) {
+            requests[tabId] = {};
+        }
+        if (!requests[tabId][hostname]) {
+            requests[tabId][hostname] = {};
+        }
+        if (!requests[tabId][hostname][type]) {
+            requests[tabId][hostname][type] = 0;
+        }
+        requests[tabId][hostname][type] += 1;
+        return browser.storage.local.set({'requests': requests});
+    });
 };
 
 var clearRequests = function(tabId) {
-    if (requests[tabId]) {
-        delete requests[tabId];
-    }
+    return getRequests().then(requests => {
+        if (requests[tabId]) {
+            delete requests[tabId];
+        }
+        return browser.storage.local.set({'requests': requests});
+    });
 };
 
 var getCurrentTab = function() {
@@ -67,36 +80,39 @@ var getCurrentTab = function() {
 
 browser.runtime.onMessage.addListener((msg, sender) => {
     if (msg.type === 'get') {
-        return getCurrentTab().then(tab => {
+        return Promise.all([
+            getCurrentTab(),
+            getRules(),
+            getRequests(),
+        ]).then(([tab, rules, requests]) => {
             var context = msg.data || getHostname(tab.url);
             return {
                 context: context,
-                rules: restrictRules(context),
+                rules: restrictRules(rules, context),
                 requests: requests[tab.id] || {},
             };
         });
     } else if (msg.type === 'setRule') {
-        setRule(
+        return setRule(
             msg.data.context,
             msg.data.hostname,
             msg.data.type,
             msg.data.value,
-        );
-        return Promise.resolve(restrictRules(msg.data.context));
+        ).then(getRules).then(rules => {
+            return restrictRules(rules, msg.data.context);
+        });
     } else if (msg.type === 'getAllRules') {
-        return Promise.resolve(rules);
+        return getRules();
     } else if (msg.type === 'setAllRules') {
-        rules = msg.data;
-        browser.storage.local.set({'rules': rules});
-        return Promise.resolve();
+        return browser.storage.local.set({'rules': msg.data});
     } else if (msg.type === 'securitypolicyviolation') {
-        pushRequest(sender.tab.id, 'inline', msg.data);
+        return pushRequest(sender.tab.id, 'inline', msg.data);
     }
 });
 
 browser.tabs.onRemoved.addListener(clearRequests);
 browser.webNavigation.onBeforeNavigate.addListener(details => {
-    clearRequests(details.tabId);
+    return clearRequests(details.tabId);
 });
 
 browser.webRequest.onBeforeRequest.addListener(details => {
@@ -112,49 +128,50 @@ browser.webRequest.onBeforeRequest.addListener(details => {
     var hostname = getHostname(details.url);
     var type = shared.TYPE_MAP[details.type] || 'other';
 
-    pushRequest(details.tabId, hostname, type);
-
-    if (!shared.shouldAllow(rules, context, hostname, type)) {
-        if (details.type === 'sub_frame') {
-            return {redirectUrl: 'data:,' + encodeURIComponent(details.url)};
-        } else {
-            return {cancel: true};
+    return Promise.all([
+        pushRequest(details.tabId, hostname, type),
+        getRules(),
+    ]).then(([_, rules]) => {
+        if (!shared.shouldAllow(rules, context, hostname, type)) {
+            if (details.type === 'sub_frame') {
+                return {redirectUrl: 'data:,' + encodeURIComponent(details.url)};
+            } else {
+                return {cancel: true};
+            }
         }
-    }
+    });
 }, {urls: ['<all_urls>']}, ['blocking']);
 
 browser.webRequest.onHeadersReceived.addListener(function(details) {
-    var context = getHostname(details.url);
+    return getRules().then(rules => {
+        var context = getHostname(details.url);
 
-    var header = type => {
-        if (shared.shouldAllow(rules, context, 'inline', type)) {
-            return 'Content-Security-Policy-Report-Only';
-        } else {
-            return 'Content-Security-Policy';
-        }
-    };
+        var header = type => {
+            if (shared.shouldAllow(rules, context, 'inline', type)) {
+                return 'Content-Security-Policy-Report-Only';
+            } else {
+                return 'Content-Security-Policy';
+            }
+        };
 
-    details.responseHeaders.push({
-        name: header('css'),
-        value: "style-src 'self' *",
-    });
-    details.responseHeaders.push({
-        name: header('script'),
-        value: "script-src 'self' *",
-    });
-    details.responseHeaders.push({
-        name: header('media'),
-        value: "img-src 'self' *",
-    });
+        details.responseHeaders.push({
+            name: header('css'),
+            value: "style-src 'self' *",
+        });
+        details.responseHeaders.push({
+            name: header('script'),
+            value: "script-src 'self' *",
+        });
+        details.responseHeaders.push({
+            name: header('media'),
+            value: "img-src 'self' *",
+        });
 
-    return {
-        responseHeaders: details.responseHeaders,
-    };
+        return {
+            responseHeaders: details.responseHeaders,
+        };
+    });
 }, {
     urls: ['<all_urls>'],
     types: ['main_frame'],
 }, ['blocking', 'responseHeaders']);
-
-browser.storage.local.get('rules').then(stored => {
-    rules = stored.rules || {};
-});
