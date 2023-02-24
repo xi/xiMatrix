f8dc166f0567b7cb623fae2eff264c09ff0c87ad
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-02-24 18:11:58 +0100

    allow to edit savedRules in settings

--- src/settings.js
+++ src/settings.js
@@ -1,15 +1,22 @@
 var form = document.querySelector('form');
-var textarea = document.querySelector('textarea');
+var textarea1 = document.querySelector('textarea.rules');
+var textarea2 = document.querySelector('textarea.savedRules');
 
-browser.storage.local.get('rules').then(data => {
+browser.storage.local.get(['rules', 'savedRules']).then(data => {
 	var rules = data.rules || {};
-	textarea.value = JSON.stringify(rules, null, 2)
+	var savedRules = data.savedRules || {};
+	textarea1.value = JSON.stringify(rules, null, 2)
+	textarea2.value = JSON.stringify(savedRules, null, 2)
 });
 
 form.addEventListener('submit', event => {
 	event.preventDefault();
-	var rules = JSON.parse(textarea.value);
-	browser.storage.local.set({'rules': rules}).then(() => {
+	var rules = JSON.parse(textarea1.value);
+	var savedRules = JSON.parse(textarea2.value);
+	browser.storage.local.set({
+		'rules': rules,
+		'savedRules': savedRules,
+	}).then(() => {
 		location.reload();
 	});
 });
