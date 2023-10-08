d604bab9f99667c52215bcd94ef63cd82f1935cd
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-10-08 18:17:00 +0200

    adapt to CSP 3
    https://w3c.github.io/webappsec-csp/

--- src/content.js
+++ src/content.js
@@ -2,12 +2,17 @@
 
 const TYPE_MAP = {
     'style-src': 'css',
+    'style-src-elem': 'css',
+    'style-src-attr': 'css',
     'script-src': 'script',
+    'script-src-elem': 'script',
+    'script-src-attr': 'script',
     'img-src': 'media',
+    'media-src': 'media',
 };
 
 document.addEventListener('securitypolicyviolation', event => {
-    var type = TYPE_MAP[event.violatedDirective];
+    var type = TYPE_MAP[event.effectiveDirective];
     if (type) {
         browser.runtime.sendMessage({
             type: 'securitypolicyviolation',
