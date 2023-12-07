5f513e070570bffda4cfad4d7d2c7c80dfb2d788
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-12-07 20:54:29 +0100

    lint: consistent indentation

--- src/settings.js
+++ src/settings.js
@@ -5,20 +5,20 @@ var textarea1 = document.querySelector('textarea.rules');
 var textarea2 = document.querySelector('textarea.savedRules');
 
 browser.storage.local.get(['rules', 'savedRules']).then(data => {
-	var rules = data.rules || {};
-	var savedRules = data.savedRules || {};
-	textarea1.value = JSON.stringify(rules, null, 2);
-	textarea2.value = JSON.stringify(savedRules, null, 2);
+    var rules = data.rules || {};
+    var savedRules = data.savedRules || {};
+    textarea1.value = JSON.stringify(rules, null, 2);
+    textarea2.value = JSON.stringify(savedRules, null, 2);
 });
 
 form.addEventListener('submit', event => {
-	event.preventDefault();
-	var rules = JSON.parse(textarea1.value);
-	var savedRules = JSON.parse(textarea2.value);
-	browser.storage.local.set({
-		'rules': rules,
-		'savedRules': savedRules,
-	}).then(() => {
-		location.reload();
-	});
+    event.preventDefault();
+    var rules = JSON.parse(textarea1.value);
+    var savedRules = JSON.parse(textarea2.value);
+    browser.storage.local.set({
+        'rules': rules,
+        'savedRules': savedRules,
+    }).then(() => {
+        location.reload();
+    });
 });
