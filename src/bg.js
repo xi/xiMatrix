4c4576a713158d9bbad11f211b69ce9194d5539f
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-12-08 00:02:36 +0100

    use async/await syntax

--- src/bg.js
+++ src/bg.js
@@ -22,88 +22,83 @@ var getHostname = function(url) {
     return u.hostname;
 };
 
-var storageGet = function(key) {
-    return STORAGE_AREAS[key].get(key).then(data => {
-        return data[key] ?? STORAGE_DEFAULTS[key];
-    });
+var storageGet = async function(key) {
+    var data = await STORAGE_AREAS[key].get(key);
+    return data[key] ?? STORAGE_DEFAULTS[key];
 };
 
-var _storageChange = function(key, fn) {
-    return storageGet(key).then(oldValue => {
-        var data = {};
-        data[key] = fn(oldValue);
-        return STORAGE_AREAS[key].set(data);
-    });
+var _storageChange = async function(key, fn) {
+    var oldValue = await storageGet(key);
+    var data = {};
+    data[key] = fn(oldValue);
+    await STORAGE_AREAS[key].set(data);
 };
 
-var storageChange = function(key, fn) {
+var storageChange = async function(key, fn) {
     lock = lock.then(() => _storageChange(key, fn));
-    return lock;
+    await lock;
 };
 
-var setRule = function(context, hostname, type, rule) {
-    return storageGet('savedRules').then(savedRules => {
-        return storageChange('rules', rules => {
-            if (hostname === 'first-party') {
-                context = '*';
+var setRule = async function(context, hostname, type, rule) {
+    var savedRules = await storageGet('savedRules');
+    await storageChange('rules', rules => {
+        if (hostname === 'first-party') {
+            context = '*';
+        }
+        if (!rules[context]) {
+            rules[context] = savedRules[context] || {};
+        }
+        if (!rules[context][hostname]) {
+            rules[context][hostname] = {};
+        }
+        if (rule) {
+            rules[context][hostname][type] = rule;
+        } else {
+            delete rules[context][hostname][type];
+            if (Object.keys(rules[context][hostname]).length === 0) {
+                delete rules[context][hostname];
             }
-            if (!rules[context]) {
-                rules[context] = savedRules[context] || {};
+            if (Object.keys(rules[context]).length === 0 && !savedRules[context]) {
+                delete rules[context];
             }
-            if (!rules[context][hostname]) {
-                rules[context][hostname] = {};
-            }
-            if (rule) {
-                rules[context][hostname][type] = rule;
-            } else {
-                delete rules[context][hostname][type];
-                if (Object.keys(rules[context][hostname]).length === 0) {
-                    delete rules[context][hostname];
-                }
-                if (Object.keys(rules[context]).length === 0 && !savedRules[context]) {
-                    delete rules[context];
-                }
-            }
-            return rules;
-        });
+        }
+        return rules;
     });
 };
 
-var getRules = function(context) {
-    return Promise.all([
+var getRules = async function(context) {
+    var [rules, savedRules] = await Promise.all([
         storageGet('rules'),
         storageGet('savedRules'),
-    ]).then(([rules, savedRules]) => {
-        var restricted = {};
-        restricted['*'] = rules['*'] || savedRules['*'] || {};
-        restricted[context] = rules[context] || savedRules[context] || {};
-        restricted.dirty = !!rules[context];
-        return restricted;
-    });
+    ]);
+    var restricted = {};
+    restricted['*'] = rules['*'] || savedRules['*'] || {};
+    restricted[context] = rules[context] || savedRules[context] || {};
+    restricted.dirty = !!rules[context];
+    return restricted;
 };
 
-var pushRequest = function(tabId, hostname, type) {
-    return storageGet('recording').then(recording => {
-        if (recording) {
-            return storageChange('requests', requests => {
-                if (!requests[tabId]) {
-                    requests[tabId] = {};
-                }
-                if (!requests[tabId][hostname]) {
-                    requests[tabId][hostname] = {};
-                }
-                if (!requests[tabId][hostname][type]) {
-                    requests[tabId][hostname][type] = 0;
-                }
-                requests[tabId][hostname][type] += 1;
-                return requests;
-            });
-        }
-    });
+var pushRequest = async function(tabId, hostname, type) {
+    var recording = await storageGet('recording');
+    if (recording) {
+        await storageChange('requests', requests => {
+            if (!requests[tabId]) {
+                requests[tabId] = {};
+            }
+            if (!requests[tabId][hostname]) {
+                requests[tabId][hostname] = {};
+            }
+            if (!requests[tabId][hostname][type]) {
+                requests[tabId][hostname][type] = 0;
+            }
+            requests[tabId][hostname][type] += 1;
+            return requests;
+        });
+    }
 };
 
-var clearRequests = function(tabId) {
-    return storageChange('requests', requests => {
+var clearRequests = async function(tabId) {
+    await storageChange('requests', requests => {
         if (requests[tabId]) {
             delete requests[tabId];
         }
@@ -111,71 +106,72 @@ var clearRequests = function(tabId) {
     });
 };
 
-var getCurrentTab = function() {
-    return browser.tabs.query({
+var getCurrentTab = async function() {
+    var tabs = await browser.tabs.query({
         active: true,
         currentWindow: true,
-    }).then(tabs => tabs[0]);
+    });
+    return tabs[0];
 };
 
-browser.runtime.onMessage.addListener((msg, sender) => {
+browser.runtime.onMessage.addListener(async (msg, sender) => {
     if (msg.type === 'get') {
-        return getCurrentTab().then(tab => {
-            var context = getHostname(tab.url);
-            return Promise.all([
-                getRules(context),
-                storageGet('requests'),
-                storageGet('recording'),
-            ]).then(([rules, requests, recording]) => {
-                return {
-                    context: context,
-                    rules: rules,
-                    requests: requests[tab.id] || {},
-                    recording: recording,
-                };
-            });
-        });
+        const tab = await getCurrentTab();
+        const context = getHostname(tab.url);
+        const [rules, requests, recording] = await Promise.all([
+            getRules(context),
+            storageGet('requests'),
+            storageGet('recording'),
+        ]);
+        return {
+            context: context,
+            rules: rules,
+            requests: requests[tab.id] || {},
+            recording: recording,
+        };
     } else if (msg.type === 'setRule') {
-        return setRule(
+        await setRule(
             msg.data.context,
             msg.data.hostname,
             msg.data.type,
             msg.data.value,
-        ).then(() => getRules(msg.data.context));
+        );
+        return await getRules(msg.data.context);
     } else if (msg.type === 'commit') {
-        var r;
-        return storageChange('rules', rules => {
+        let r;
+        await storageChange('rules', rules => {
             r = rules[msg.data];
             delete rules[msg.data];
             return rules;
-        }).then(() => storageChange('savedRules', savedRules => {
+        });
+        await storageChange('savedRules', savedRules => {
             if (Object.keys(r).length === 0) {
                 delete savedRules[msg.data];
             } else {
                 savedRules[msg.data] = r;
             }
             return savedRules;
-        }));
+        });
     } else if (msg.type === 'reset') {
-        return storageChange('rules', rules => {
+        await storageChange('rules', rules => {
             delete rules[msg.data];
             return rules;
         });
     } else if (msg.type === 'securitypolicyviolation') {
-        return pushRequest(sender.tab.id, 'inline', msg.data);
+        await pushRequest(sender.tab.id, 'inline', msg.data);
     } else if (msg.type === 'toggleRecording') {
-        return storageChange('recording', recording => !recording);
+        await storageChange('recording', recording => !recording);
     }
 });
 
 browser.tabs.onRemoved.addListener(clearRequests);
-browser.webNavigation.onBeforeNavigate.addListener(details => {
+browser.webNavigation.onBeforeNavigate.addListener(async details => {
     if (details.frameId === 0) {
-        return clearRequests(details.tabId);
+        await clearRequests(details.tabId);
     }
 });
 
-browser.webRequest.onBeforeSendHeaders.addListener(details => {
+browser.webRequest.onBeforeSendHeaders.addListener(async details => {
     var context = getHostname(details.documentUrl || details.url);
     if (details.frameAncestors && details.frameAncestors.length) {
         var last = details.frameAncestors.length - 1;
@@ -197,55 +193,53 @@ browser.webRequest.onBeforeSendHeaders.addListener(details => {
         promises.push(pushRequest(details.tabId, hostname, 'cookie'));
     }
 
-    return Promise.all(promises).then(([rules, ...rest]) => {
-        if (
-            details.type !== 'main_frame'
-            && !shared.shouldAllow(rules, context, hostname, type)
-        ) {
-            if (details.type === 'sub_frame') {
-                // this can in turn be blocked by a local CSP
-                return {redirectUrl: 'data:,' + encodeURIComponent(details.url)};
-            } else {
-                return {cancel: true};
-            }
-        }
-
-        if (shared.shouldAllow(rules, context, hostname, 'cookie')) {
-            return {requestHeaders: details.requestHeaders};
+    var [rules, ...rest] = await Promise.all(promises);
+    if (
+        details.type !== 'main_frame'
+        && !shared.shouldAllow(rules, context, hostname, type)
+    ) {
+        if (details.type === 'sub_frame') {
+            // this can in turn be blocked by a local CSP
+            return {redirectUrl: 'data:,' + encodeURIComponent(details.url)};
         } else {
-            var filtered = details.requestHeaders.filter(h => !isCookie(h));
-            return {requestHeaders: filtered};
+            return {cancel: true};
         }
-    });
+    }
+
+    if (shared.shouldAllow(rules, context, hostname, 'cookie')) {
+        return {requestHeaders: details.requestHeaders};
+    } else {
+        var filtered = details.requestHeaders.filter(h => !isCookie(h));
+        return {requestHeaders: filtered};
+    }
 }, {urls: ['<all_urls>']}, ['blocking', 'requestHeaders']);
 
-browser.webRequest.onHeadersReceived.addListener(details => {
+browser.webRequest.onHeadersReceived.addListener(async details => {
     var context = getHostname(details.url);
-    return Promise.all([
+    var [rules, recording] = await Promise.all([
         getRules(context),
         storageGet('recording'),
-    ]).then(([rules, recording]) => {
-        var csp = (type, value) => {
-            var name = 'Content-Security-Policy';
-            if (shared.shouldAllow(rules, context, 'inline', type)) {
-                if (recording) {
-                    name = 'Content-Security-Policy-Report-Only';
-                } else {
-                    return;
-                }
+    ]);
+    var csp = (type, value) => {
+        var name = 'Content-Security-Policy';
+        if (shared.shouldAllow(rules, context, 'inline', type)) {
+            if (recording) {
+                name = 'Content-Security-Policy-Report-Only';
+            } else {
+                return;
             }
-            details.responseHeaders.push({
-                name: name,
-                value: value,
-            });
-        };
+        }
+        details.responseHeaders.push({
+            name: name,
+            value: value,
+        });
+    };
 
-        csp('css', "style-src 'self' *");
-        csp('script', "script-src 'self' *");
-        csp('media', "img-src 'self' *");
+    csp('css', "style-src 'self' *");
+    csp('script', "script-src 'self' *");
+    csp('media', "img-src 'self' *");
 
-        return {responseHeaders: details.responseHeaders};
-    });
+    return {responseHeaders: details.responseHeaders};
 }, {
     urls: ['<all_urls>'],
     types: ['main_frame'],
