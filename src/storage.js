ba4566615cba9ee6a9d69e16072fea3945b44cfc
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-12-08 20:34:31 +0100

    cache storage access
    I used http://www.raymondhill.net/ublock/pageloadspeed.html
    for a simple benchmark:
    
    without cache (all allowed)	3543.45 ms
    no extension	3030.00 ms
    with cache (all allowed)	2536.27 ms
    without cache (all blocked)	782.36 ms
    with cache (all blocked	732.91 ms
    
    I am not sure how valid these results are, but it is good enough to give
    it a try.

--- src/storage.js
+++ src/storage.js
@@ -14,16 +14,25 @@ var STORAGE_AREAS = {
 };
 
 var lock = Promise.resolve();
+var cache = {};
 
-export var get = async function(key) {
+var _get = async function(key) {
     var data = await STORAGE_AREAS[key].get(key);
     return data[key] ?? STORAGE_DEFAULTS[key];
+};
+
+export var get = function(key) {
+    if (!cache[key]) {
+        cache[key] = _get(key);
+    }
+    return cache[key];
 };
 
 var _change = async function(key, fn) {
     var oldValue = await get(key);
     var data = {};
     data[key] = fn(oldValue);
+    delete cache[key];
     await STORAGE_AREAS[key].set(data);
 };
 
@@ -31,6 +40,14 @@ export var change = async function(key, fn) {
     lock = lock.then(() => _change(key, fn));
     await lock;
 };
+
+var invalidateCache = function(changes) {
+  for (var key in changes) {
+    delete cache[key];
+  }
+};
+
+browser.storage.local.onChanged.addListener(invalidateCache);
 
 // migrations
 browser.runtime.onInstalled.addListener(() => {
